02331afcb95b112a8d8a86cdedd543ceacb64b72
Aiman Fatima <aimanfatimadl@gmail.com>
2022-01-30 22:15:44 +0530

    Custom wrapper comp: avoid div soup

--- user-management-app/src/components/Users/AddUser.js
+++ user-management-app/src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
@@ -44,7 +45,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -72,7 +73,7 @@ const AddUser = (props) => {
         </form>
       </Card>
       )
-    </div>
+    </Wrapper>
   );
 };
 
