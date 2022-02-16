7de0612693623cbdd8a81560f2f741d51fa2a256
Aiman Fatima <aimanfatimadl@gmail.com>
2022-02-16 21:44:29 +0530

    Use the context

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,22 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart.context";
 
 const HeaderCartButton = (props) => {
-  return (
-    <button className={classes.button} onClick={props.onClick}>
-      <span className={classes.icon}>
-        <CartIcon />
-      </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
-    </button>
-  );
+	const cartCtx = useContext(CartContext);
+	const noOfCartItems = cartCtx.items.reduce((currentNo, item) => {
+		return currentNo + item.amount;
+	}, 0);
+	return (
+		<button className={classes.button} onClick={props.onClick}>
+			<span className={classes.icon}>
+				<CartIcon />
+			</span>
+			<span>Your Cart</span>
+			<span className={classes.badge}>{noOfCartItems}</span>
+		</button>
+	);
 };
 
 export default HeaderCartButton;
