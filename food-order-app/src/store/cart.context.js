1793808aacebbccbcae3b9ce1c3614f4839edee7
Aiman Fatima <aimanfatimadl@gmail.com>
2022-02-16 20:02:22 +0530

    Add Cart Context

---
+++ food-order-app/src/store/cart.context.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CartContext = React.createContext({
+	items: [],
+	totalAmount: 0,
+	addItem: (items) => {},
+	removeItem: (id) => {},
+});
+
+export default CartContext;
