c2860dd988ff2c1a75e845d80c13259447af9f2b
Aiman Fatima <aimanfatimadl@gmail.com>
2022-02-17 00:26:23 +0530

    Use useEffect hook

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart.context";
 
 const HeaderCartButton = (props) => {
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 	const cartCtx = useContext(CartContext);
-	const noOfCartItems = cartCtx.items.reduce((currentNo, item) => {
+	const { items } = cartCtx;
+	const noOfCartItems = items.reduce((currentNo, item) => {
 		return currentNo + item.amount;
 	}, 0);
+
+	const btnClasses = `${classes.button} ${
+		btnIsHighlighted ? classes.bump : ""
+	}`;
+	useEffect(() => {
+		if (cartCtx.items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
